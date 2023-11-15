e47a64db076ef0e3b5fe3e3b9772520ad59770cb
Prasanth Dasika <prasanthdasika@gmail.com>
2023-11-14 23:44:37 -0500

    Reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPractice';
+  tweet = {
+    body: '...',
+    likesCount: 10,
+    isActive: true
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { CoursesServiceService } from './course/courses-service.service';
 import { AuthorComponent } from './author/author.component';
 import { AuthorService } from './author/author.service';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { LikeComponent } from './like/like.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     AuthorComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    LikeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [CoursesServiceService, AuthorService],
   bootstrap: [AppComponent]
